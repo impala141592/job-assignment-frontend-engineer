272d8266c612c4f277654cb5887622caaa005754
impala141592 <dziedziochs@gmail.com>
2026-07-30 11:26:31 +0200

    refactor: make auth page route aware

--- src/LoginRegister.tsx
+++ src/LoginRegister.tsx
@@ -1,30 +1,57 @@
+import { Link, useLocation } from "react-router-dom";
+
 export default function LoginRegister() {
+  const { pathname } = useLocation();
+  const isLogin = pathname === "/login";
+
   return (
     <>
       <div className="auth-page">
         <div className="container page">
           <div className="row">
             <div className="col-md-6 offset-md-3 col-xs-12">
-              <h1 className="text-xs-center">Sign up</h1>
+              <h1 className="text-xs-center">
+                {isLogin ? "Sign in" : "Sign up"}
+              </h1>
+
               <p className="text-xs-center">
-                <a href="">Have an account?</a>
+                {isLogin ? (
+                  <Link to="/register">Need an account?</Link>
+                ) : (
+                  <Link to="/login">Have an account?</Link>
+                )}
               </p>
 
-              <ul className="error-messages">
-                <li>That email is already taken</li>
-              </ul>
-
               <form>
+                {!isLogin && (
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
+                      type="text"
+                      placeholder="Your Name"
+                    />
+                  </fieldset>
+                )}
+
                 <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Your Name" />
+                  <input
+                    className="form-control form-control-lg"
+                    type="email"
+                    placeholder="Email"
+                  />
                 </fieldset>
+
                 <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="text" placeholder="Email" />
+                  <input
+                    className="form-control form-control-lg"
+                    type="password"
+                    placeholder="Password"
+                  />
                 </fieldset>
-                <fieldset className="form-group">
-                  <input className="form-control form-control-lg" type="password" placeholder="Password" />
-                </fieldset>
-                <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+
+                <button className="btn btn-lg btn-primary pull-xs-right">
+                  {isLogin ? "Sign in" : "Sign up"}
+                </button>
               </form>
             </div>
           </div>
@@ -33,12 +60,16 @@ export default function LoginRegister() {
 
       <footer>
         <div className="container">
-          <a href="/#" className="logo-font">
+          <Link to="/" className="logo-font">
             conduit
-          </a>
+          </Link>
+
           <span className="attribution">
-            An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
-            licensed under MIT.
+            An interactive learning project from{" "}
+            <a href="https://thinkster.io">
+              Thinkster
+            </a>
+            . Code &amp; design licensed under MIT.
           </span>
         </div>
       </footer>
